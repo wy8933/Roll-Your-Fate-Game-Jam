519505303b21e719f47b914bd096e6a1095e0a84
Weijie Ye <wy8933@rit.edu>
2025-11-08 18:52:02 -0500

    Done with note collection description

--- Roll Your Fate Game Jam/Assets/Scripts/UI/NoteCollectionUIManager.cs
+++ Roll Your Fate Game Jam/Assets/Scripts/UI/NoteCollectionUIManager.cs
@@ -9,9 +9,11 @@ public class NoteCollectionUIManager : MonoBehaviour
     public TMP_Text noteText;
 
     public List<NoteItem> noteItems = new List<NoteItem>();
+    CollectedStickyNoteSO collectedStickyNoteSO;
 
     public void SetUp(CollectedStickyNoteSO collectedStickyNoteSO) 
     {
+        this.collectedStickyNoteSO = collectedStickyNoteSO;
         for (int i = 0; i < 15; i++) 
         {
             bool founded = false;
@@ -28,6 +30,33 @@ public class NoteCollectionUIManager : MonoBehaviour
             {
                 noteItems[i].SetNote(i, "Not Collected");
             }
+        }
+    }
+
+    public void ShowFullNote(int id) 
+    {
+        NoteData currentData = new NoteData();
+        bool founded = false;
+
+        foreach (NoteData data in collectedStickyNoteSO.collectedNote) 
+        {
+            if (data.ID == id) 
+            {
+                currentData = data;
+                founded = true;
+                break;
+            }
+        }
+
+        if (founded)
+        {
+            noteTitle.text = currentData.Name;
+            noteText.text = currentData.Text;
+        }
+        else 
+        {
+            noteTitle.text = id + " Not Collected";
+            noteText.text = "Note not collected";
         }
     }
 }
