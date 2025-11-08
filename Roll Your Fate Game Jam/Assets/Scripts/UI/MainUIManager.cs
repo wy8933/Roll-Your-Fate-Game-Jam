cd3957d0d18e7888cb05d6f9b971874f72c873a0
Weijie Ye <wy8933@rit.edu>
2025-11-08 10:47:00 -0500

    Done with main menu

---
+++ Roll Your Fate Game Jam/Assets/Scripts/UI/MainUIManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUIManager : MonoBehaviour
+{
+    public SettingSO settingSO;
+    public GameObject settingPanel;
+    public GameObject creditPanel;
+
+    public void OpenSetting()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void CloseSetting()
+    {
+        settingPanel.SetActive(false);
+    }
+
+    public void OpenCredit()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void CloseCredit() 
+    {
+        creditPanel.SetActive(false);
+    }
+
+    public void ChangeVolume(float value) 
+    {
+        settingSO.audioVolume = value;
+    }
+
+    public void ChangeSensitivity(float value)
+    {
+        settingSO.mouseSensitivity = value;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CloseGame() 
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Roll Your Fate Game Jam/Assets/Scripts/Utility/SettingSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SettingSO", menuName = "SettingSO")]
+public class SettingSO : ScriptableObject
+{
+    public float mouseSensitivity;
+    public float audioVolume;
+}
