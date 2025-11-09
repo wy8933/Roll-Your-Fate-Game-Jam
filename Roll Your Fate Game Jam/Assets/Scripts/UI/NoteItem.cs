2d5700e8000156b1882824d4c318ce033bff64e0
Weijie Ye <wy8933@rit.edu>
2025-11-09 13:59:30 -0500

    Fixed Note Collection UI not work with controller

--- Roll Your Fate Game Jam/Assets/Scripts/UI/NoteItem.cs
+++ Roll Your Fate Game Jam/Assets/Scripts/UI/NoteItem.cs
@@ -1,14 +1,23 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NoteItem : MonoBehaviour
 {
     public TMP_Text noteID;
     public TMP_Text noteTitle;
 
+    [SerializeField] private float _selectedScale = 1.5f;
+
     public void SetNote(int id, string name) 
     {
         noteID.text = "LOG " + id.ToString();
         noteTitle.text = name;   
+    }
+
+    public void SetSelected(bool selected)
+    {
+        if (noteTitle != null)
+            noteTitle.rectTransform.localScale = selected ? Vector3.one * _selectedScale : Vector3.one;
     }
 }
