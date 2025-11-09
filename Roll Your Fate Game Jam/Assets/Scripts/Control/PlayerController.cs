71b76840ae01d2bef62075299b00a1d9192b076c
hsl2363 <hshaolinv@sina.com>
2025-11-09 13:22:04 -0500

    Ground Detection

--- Roll Your Fate Game Jam/Assets/Scripts/Control/PlayerController.cs
+++ Roll Your Fate Game Jam/Assets/Scripts/Control/PlayerController.cs
@@ -22,15 +22,19 @@ namespace Control
         [SerializeField]
         private CharacterMovementParameter movementParam;
         private float Acceleration => movementParam.acceleration;
+        private float Acceleration_Air => movementParam.acceleration_Air;
         private float MaxSpeed => movementParam.maxSpeed;
         private float Gravity => movementParam.gravity;
         private float BrakeFactor => movementParam.brakeFactor;
         private float RotateSpeed => movementParam.rotateSpeed;
-
+        private Vector3 GroundDetectionCenter => movementParam.groundDetectionCenter;
+        private float GroundDetectionRadius => movementParam.groundDetectionRadius;
+        
         #endregion
 
         #region Flags
 
+        public bool isOnGround;
         #endregion
 
         #region AudioClips
@@ -59,8 +63,8 @@ namespace Control
 
         private void FixedUpdate()
         {
+            GroundCheck();
             Move();
-            
         }
 
         #region Movement
@@ -70,15 +74,25 @@ namespace Control
             Vector2 targetDirection = PlayerInputHandler.Instance.InputVector.normalized;
             Vector3 velocity = RB.linearVelocity;
             Vector2 horizontalVel = new Vector2(velocity.x, velocity.z);
-            if (targetDirection.magnitude > eps)
+            if (isOnGround)
             {
-                horizontalVel = Vector2.ClampMagnitude(horizontalVel + targetDirection * Acceleration * Time.fixedDeltaTime, MaxSpeed * PlayerInputHandler.Instance.InputVector.magnitude); 
+                if (targetDirection.magnitude > eps)
+                {
+                    horizontalVel = Vector2.ClampMagnitude(horizontalVel + targetDirection * Acceleration * Time.fixedDeltaTime, MaxSpeed * PlayerInputHandler.Instance.InputVector.magnitude); 
+                }
+                else
+                {
+                    horizontalVel *= BrakeFactor;
+                    if (horizontalVel.magnitude < eps)
+                        horizontalVel = Vector2.zero;
+                }
             }
             else
             {
-                horizontalVel *= BrakeFactor;
-                if (horizontalVel.magnitude < eps)
-                    horizontalVel = Vector2.zero;
+                if (targetDirection.magnitude > eps)
+                {
+                    horizontalVel = Vector2.ClampMagnitude(horizontalVel + targetDirection * Acceleration_Air * Time.fixedDeltaTime, MaxSpeed * PlayerInputHandler.Instance.InputVector.magnitude); 
+                }
             }
             animator.SetFloat("Speed", horizontalVel.magnitude);
             velocity = new Vector3(horizontalVel.x, velocity.y - Gravity * Time.fixedDeltaTime, horizontalVel.y);
@@ -96,6 +110,19 @@ namespace Control
             
         }
 
+        public void GroundCheck()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position + GroundDetectionCenter, GroundDetectionRadius);
+            foreach (var collider in colliders)
+            {
+                if (collider.CompareTag("Player"))
+                    continue;
+                isOnGround = true;
+                return;
+            }
+            isOnGround = false;
+        }
+
         #endregion
 
         public void EnableControl()
@@ -108,6 +135,14 @@ namespace Control
             PlayerInputHandler.Instance.Disable();
         }
 
+        #region Gizmos
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position + GroundDetectionCenter, GroundDetectionRadius);
+        }
+
+        #endregion
     }
 
 
@@ -115,9 +150,12 @@ namespace Control
     public class CharacterMovementParameter
     {
         public float acceleration;
+        public float acceleration_Air;
         public float maxSpeed;
         public float brakeFactor;
         public float rotateSpeed;
         public float gravity;
+        public Vector3 groundDetectionCenter;
+        public float groundDetectionRadius;
     }
 }
